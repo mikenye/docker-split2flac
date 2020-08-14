87c8d3a39059389577da191f82c27bda87d9b589
Mike Nye <mike.nye@gmail.com>
2020-08-14 09:54:58 +0800

    Prevent build caching

--- buildx.sh
+++ buildx.sh
@@ -12,7 +12,7 @@ export DOCKER_CLI_EXPERIMENTAL="enabled"
 docker buildx use homecluster
 
 # Build & push latest
-docker buildx build -t "${REPO}/${IMAGE}:latest" --compress --push --platform "${PLATFORMS}" .
+docker buildx build --no-cache -t "${REPO}/${IMAGE}:latest" --compress --push --platform "${PLATFORMS}" .
 
 # Get split2flac version
 VERSION=$(docker run --rm "${REPO}/${IMAGE}:latest" --version | head -1 | cut -d ":" -f 2 | tr -d " " | sed 's/\r$//')
